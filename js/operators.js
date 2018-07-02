c12e784a10d3d52c4388ac1d79dfa0e82cc8bb12
DaBestSmoot <jeremiah.smoot@scholar.smashprogram.org>
2018-07-02 10:32:55 -0400

    adding local files

---
+++ js/operators.js
@@ -0,0 +1,12 @@
+function ppG(){
+    
+   var miles = document.getElementById("Miles").value;
+   var mpg = document.getElementById("mpg").value;
+   var co2 = 20;
+   var gallons = miles / mpg;
+
+   var total = co2 * gallons;
+
+   document.getElementById("answer").value = total;
+    
+}
---
+++ p5js/empty-example/sketch.js
@@ -0,0 +1,46 @@
+//var Lx = 300;
+//var Uy = 300;
+//
+//function keyPressed() {
+//  if (keyCode === UP_ARROW) {
+//    Uy += 10;
+//  } else if (keyCode === DOWN_ARROW) {
+//   Uy += 10;
+//  }
+//  if (keyCode === LEFT_ARROW) {
+//    Lx += 5;
+//  } else if (keyCode === RIGHT_ARROW) {
+//    Lx +=  5;
+//  }
+//}
+//
+//function setup() {
+//  createCanvas(400, 400, WEBGL);
+//}
+//
+//function draw() {
+//  background(220);
+////    translate(Lx,Uy);
+//  sphere(40);
+//}
+//
+////function setup() { 
+////  createCanvas(400, 400);
+////} 
+////
+////function draw() { 
+////  background(220);
+////  fill(0);
+////  ellipse(x,y,50,50);
+////  
+////  //x = x + 1;
+//// 
+////    
+////  if (x >= 400){
+////   x = 0; 
+////  }
+//// 
+////}
+//
+//
+//  
